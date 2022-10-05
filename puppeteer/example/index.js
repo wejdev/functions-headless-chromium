579e1e7daa6759b698a15975ed4451da9104cd97
billj <willard.johnson@eso.com>
2022-10-05 11:56:02 -0700

    FINC-5119 Puppeteer running with Azure function app

---
+++ puppeteer/example/index.js
@@ -0,0 +1,20 @@
+const puppeteer = require("puppeteer");
+
+module.exports = async function (context, req) {
+    const url = req.query.url || "https://google.com/";
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const screenshotBuffer = await page.screenshot(
+        {
+            fullPage: true
+        });
+    await browser.close();
+
+    context.res = {
+        body: screenshotBuffer,
+        headers: {
+            "content-type": "image/png"
+        }
+    };
+};
--- puppeteer/screenshot/index.js
+++ puppeteer/screenshot/index.js
@@ -1,17 +1,64 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
 module.exports = async function (context, req) {
-    const url = req.query.url || "https://google.com/";
+    const headerTemplate = `<span></span>`;
+
+    const footerTemplate = `
+        <html>
+            <head>
+                <style type="text/css">
+                    #footer {
+                    padding: 0;
+                    }
+                    .content-footer {
+                    width: 100%;
+                    background-color: brown;
+                    color: white;
+                    padding: 5px;
+                    print-color-adjust: exact;
+                    -webkit-print-color-adjust: exact;
+                    vertical-align: middle;
+                    font-size: 10pt;
+                    margin-top: 0;
+                    display: inline-block;
+                    text-align: center;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="content-footer">
+                    Page <span class="pageNumber" style="background-color: hotpink"></span> of <span class="totalPages" style="background-color: lime"></span>
+                </div>
+            </body>
+        </html>
+    `;
+
+    const url = req.query.url || 'https://eso.com/';
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url);
-    const screenshotBuffer = await page.screenshot({ fullPage: true });
+    await page.goto(url, {
+        waitUntil: 'load',
+        timeout: 0
+    });
+
+    const screenshotBuffer = await page.pdf({
+        // fullPage: true,
+        displayHeaderFooter: true,
+        // headerTemplate: headerTemplate,
+        footerTemplate: footerTemplate,
+        format: 'letter',
+        margin: {
+            top: '60px',
+            bottom: '60px'
+        },
+        printBackground: true
+    });
     await browser.close();
 
     context.res = {
         body: screenshotBuffer,
         headers: {
-            "content-type": "image/png"
+            'content-type': 'application/pdf'
         }
     };
 };
